179dee15c8c87744db01334ab1cf47ce70ac3faa
victordantasdev <victordantasdeveloper@gmail.com>
2023-01-17 20:49:38 -0300

    feat: adds argon to hash the password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) { }
 
-  signup(dto: AuthDto) {
-    console.log({ dto })
+  async signup(dto: AuthDto) {
+    const { email, password } = dto;
 
-    return { message: 'I\'ve signed up' };
+    // generate password hash
+    const hash = await argon.hash(password);
+
+    // save user to db
+    const user = await this.prisma.user.create({
+      data: {
+        email,
+        hash,
+      }
+    });
+
+    delete user.hash;
+
+    // return the saved user
+    return user;
   }
 
   signin() {
