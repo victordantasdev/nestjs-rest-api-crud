4442a46204460857d7abe2fcfc511dd77a80ab40
victordantasdev <victordantasdeveloper@gmail.com>
2023-01-17 20:43:25 -0300

    feat: adds global pipes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) { }
 
-  signup() {
+  signup(dto: AuthDto) {
+    console.log({ dto })
+
     return { message: 'I\'ve signed up' };
   }
 
